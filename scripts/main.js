20b25b31a10e79830590238e381f43ed4c5b504e
Justin Lung <justin.lung@hva.nl>
2021-11-26 16:26:19 +0100

    WIP Animations removed

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,17 @@
-const observer = new IntersectionObserver(showHeadings); // New Intersection object
-const elements = document.querySelectorAll('main > section');
+// const observer = new IntersectionObserver(showHeadings); // New Intersection object
+// const elements = document.querySelectorAll('main > section');
 
-elements.forEach(element => {
-    observer.observe(element);
-})
+// elements.forEach(element => {
+//     observer.observe(element);
+// })
 
-function showHeadings(entries, observer) {
-    entries.forEach(entry => {
-        const targetClass = entry.target.classList;
-        if (entry.isIntersecting) {
-            targetClass.add('observed')
-        } else {
-            targetClass.remove('observed')
-        }
-    });
-};
+// function showHeadings(entries, observer) {
+//     entries.forEach(entry => {
+//         const targetClass = entry.target.classList;
+//         if (entry.isIntersecting) {
+//             targetClass.add('observed')
+//         } else {
+//             targetClass.remove('observed')
+//         }
+//     });
+// };
