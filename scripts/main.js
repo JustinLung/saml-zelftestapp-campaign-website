ab4a1019ea685fef22678c5cd838ee84d3ee968d
M4TThys123 <70703948+M4TThys123@users.noreply.github.com>
2021-11-29 11:41:25 +0100

    changes made

--- scripts/main.js
+++ scripts/main.js
@@ -11,3 +11,16 @@
 //         if (entry.isIntersecting) targetClass.add('observed')
 //     });
 // };
+const primaryNav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+navToggle.addEventListener('click', () => {
+    const visibility = primaryNav.getAttribute("data-visible");
+
+    if (visibility === "false") {
+        primaryNav.setAttribute("data-visible", true)   
+    } else if (visibility === "true") {
+        primaryNav.setAttribute("data-visible", false)   
+    }
+});
+
